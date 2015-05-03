59b70242ee09f4075e271619c31328708c7729a4
Tim De Pauw <hi@tmdpw.eu>
2015-05-03 17:33:47 +0200

    Isolating some more logic; preferring "kodi" config over "xbmc"

--- kodi-missing-episodes.js
+++ kodi-missing-episodes.js
@@ -1,6 +1,4 @@
-var XBMC = require('xbmc'),
-    TVDB = require('tvdb'),
-    Q = require('q'),
+var Q = require('q'),
     qlimit = require('qlimit'),
     yargs = require('yargs'),
     _ = require('lodash'),
@@ -8,13 +6,18 @@ var XBMC = require('xbmc'),
 
 Q.longStackSupport = true;
 
-var xbmc, tvdbs;
 var events = new EventEmitter();
 var config = require('./config.json');
 config.options = _.assign(config.options || {}, yargs.argv);
 
+var context = {
+  events: events,
+  config: config
+};
 var format = config.options.format || 'text';
-require('./lib/output/' + format)(events, config);
+require('./lib/output/' + format).call(context);
+var kodi = require('./lib/kodi').call(context);
+var tvdb = require('./lib/tvdb').call(context);
 
 var compareNum = function(a, b) {
   return a - b;
@@ -28,125 +31,144 @@ var isFutureDate = function(date) {
   return (new Date(date) > new Date());
 };
 
-var tvdbGetEpisodes = function(title) {
-  events.emit('tvdb_finding_show', {title: title});
-  var tvdb = null;
-  return tvdbs.reduce(function(prev, curr) {
-    return prev.then(function(results) {
-      if (results.length) {
-        return results;
-      }
-      tvdb = curr;
-      return Q.ninvoke(curr, 'findTvShow', title);
-    });
-  }, Q([]))
-  .then(function(results) {
-    if (!results.length) {
-      throw new Error('Show not found: ' + title);
-    }
-    events.emit('tvdb_found_show', {
-      title: title,
-      id: results[0].id,
-      language: results[0].language
-    });
-    return results[0];
-  })
-  .then(function(meta) {
-    events.emit('tvdb_getting_episodes', {
-      title: title,
-      id: meta.id,
-      language: meta.language
-    });
-    return Q.ninvoke(tvdb, 'getInfo', meta.id);
-  })
-  .then(function(info) {
-    events.emit('tvdb_got_episodes', {
-      title: title,
-      episodes: info.episodes
-    });
-    var tvdbEps = {};
-    info.episodes.filter(function(episode) {
-      return ((!config.options.excludeSpecials || episode.season > 0) &&
-        (config.options.includeUnaired || !isFutureDate(episode.firstAired)));
-    }).forEach(function(episode) {
-      tvdbEps[episode.season] = tvdbEps[episode.season] || {};
-      tvdbEps[episode.season][episode.number] = {
-        id: episode.id,
-        title: episode.name
-      };
-    });
-    return tvdbEps;
+var filterEpisodes = function(episodes, noSpecials, noUnaired) {
+  return episodes.filter(function(episode) {
+    return ((!noSpecials || episode.season > 0) &&
+      (!noUnaired || !isFutureDate(episode.firstAired)));
   });
 };
 
-var xbmcGetEpisodes = function(title, id) {
-  events.emit('kodi_getting_episodes', {
+var groupBySeason = function(episodes, numberKey, titleKey, idKey) {
+  var result = {};
+  episodes.forEach(function(episode) {
+    var season = episode.season, number = episode[numberKey];
+    result[season] = result[season] || {};
+    result[season][number] = {
+      id: episode[idKey],
+      title: episode[titleKey]
+    };
+  });
+  return result;
+};
+
+var tvdbInit = function() {
+  events.emit('tvdb_initializing');
+  tvdb.init();
+  events.emit('tvdb_initialized');
+};
+
+var tvdbFindShow = function(title) {
+  events.emit('tvdb_finding_show', {title: title});
+  return tvdb.findShow(title)
+    .then(function(show) {
+      events.emit('tvdb_found_show', {
+        title: title,
+        id: show.id,
+        language: show.language
+      });
+      return show;
+    });
+};
+
+var tvdbGetEpisodesForShow = function(show) {
+  var data = {
+    title: show.name,
+    id: show.id,
+    language: show.language
+  };
+  events.emit('tvdb_getting_episodes', data);
+  return tvdb.getEpisodes(show)
+    .then(function(episodes) {
+      events.emit('tvdb_got_episodes', _.assign({episodes: episodes}, data));
+      return episodes;
+    });
+};
+
+var tvdbGetEpisodes = function(title) {
+  return tvdbFindShow(title)
+    .then(tvdbGetEpisodesForShow)
+    .then(function(episodes) {
+      episodes = filterEpisodes(episodes, config.options.excludeSpecials,
+        !config.options.includeUnaired);
+      return groupBySeason(episodes, 'number', 'name', 'id');
+    });
+};
+
+var kodiConnect = function() {
+  events.emit('kodi_connecting');
+  return kodi.connect()
+    .then(function() {
+      events.emit('kodi_connected');
+    });
+};
+
+var kodiDisconnect = function() {
+  events.emit('kodi_disconnecting');
+  return kodi.disconnect()
+    .then(function() {
+      events.emit('kodi_disconnected');
+    });
+};
+
+var kodiGetEpisodes = function(title, id) {
+  var data = {
     title: title,
     id: id
-  });
-  return Q(xbmc.media.episodes(id, null, {
-    properties: ['season', 'episode', 'originaltitle']
-  }))
-  .then(function(episodes) {
-    events.emit('kodi_got_episodes', {
-      title: title,
-      episodes: episodes
+  };
+  events.emit('kodi_getting_episodes', data);
+  return kodi.getEpisodes(title, id)
+    .then(function(episodes) {
+      events.emit('kodi_got_episodes', _.assign({episodes: episodes}, data));
+      episodes = filterEpisodes(episodes, config.options.excludeSpecials);
+      return groupBySeason(filterEpisodes(episodes),
+        'episode', 'originaltitle', 'episodeid');
     });
-    var xbmcEps = {};
-    episodes.filter(function(episode) {
-      return (!config.options.excludeSpecials || episode.season > 0);
-    }).forEach(function(episode) {
-      xbmcEps[episode.season] = xbmcEps[episode.season] || {};
-      xbmcEps[episode.season][episode.episode] = {
-        id: episode.episodeid,
-        title: episode.originaltitle
-      };
-    });
-    return xbmcEps;
-  });
 };
 
-var removeOldEpisodes = function(title, tvdbEps, xbmcEps) {
-  var firstSeason = _.min(Object.keys(xbmcEps).filter(_.identity).map(toInt));
+var removeSeasonsBefore = function(firstSeason, tvdbEps) {
+  var seasons = Object.keys(tvdbEps).map(toInt)
+    .filter(function(season) {
+      return (season && season < firstSeason);
+    });
+  seasons.forEach(function(season) {
+    delete tvdbEps[season];
+  });
+  return seasons;
+};
+
+var removeEpisodesBefore = function(firstSeason, firstEpisode, tvdbEps) {
+  var episodes = Object.keys(tvdbEps[firstSeason]).map(toInt)
+    .filter(function(episode) {
+      return (episode < firstEpisode);
+    });
+  episodes.forEach(function(episode) {
+    delete tvdbEps[firstSeason][episode];
+  });
+  return episodes;
+};
+
+var removeOldEpisodes = function(title, tvdbEps, kodiEps) {
+  var firstSeason = _.min(Object.keys(kodiEps).filter(_.identity).map(toInt));
   if (firstSeason) {
-    var firstEpisode = _.min(Object.keys(xbmcEps[firstSeason]).map(toInt));
+    var firstEpisode = _.min(Object.keys(kodiEps[firstSeason]).map(toInt));
     if (firstSeason > 1 || firstEpisode > 1) {
-      events.emit('excluding_episodes_before', {
+      var data = {
         title: title,
         season: firstSeason,
         episode: firstEpisode
-      });
-      var seasons = Object.keys(tvdbEps).map(toInt).filter(function(season) {
-        return (season && season < firstSeason);
-      });
-      seasons.forEach(function(season) {
-        delete tvdbEps[season];
-      });
-      var episodes = Object.keys(tvdbEps[firstSeason]).map(toInt).filter(function(episode) {
-        return (episode < firstEpisode);
-      });
-      episodes.forEach.forEach(function(episode) {
-        delete tvdbEps[firstSeason][episode];
-      });
-      events.emit('excluded_episodes_before', {
-        title: title,
-        season: firstSeason,
-        episode: firstEpisode,
-        seasons: seasons,
-        episodes: episodes
-      });
+      };
+      events.emit('excluding_episodes_before', data);
+      data = _.assign({}, data);
+      data.seasons = removeSeasonsBefore(firstSeason, tvdbEps);
+      data.episodes = removeEpisodesBefore(firstSeason, firstEpisode, tvdbEps);
+      events.emit('excluded_episodes_before', data);
     }
   }
 };
 
-var matchEpisodeInfo = function(title, tvdbEps, xbmcEps) {
-  var missingSeasons = [], missingEpisodes = [];
-  if (config.options.excludeOlder) {
-    removeOldEpisodes(title, tvdbEps, xbmcEps);
-  }
-  var tvdbSeasons = Object.keys(tvdbEps).map(toInt);
-  tvdbSeasons.filter(function(tvdbSeason) {
-    if (!xbmcEps.hasOwnProperty(tvdbSeason)) {
+var buildSeasonFilter = function(tvdbEps, kodiEps, missingSeasons) {
+  return function(tvdbSeason) {
+    if (!kodiEps.hasOwnProperty(tvdbSeason)) {
       missingSeasons.push({
         season: tvdbSeason,
         episodes: Object.keys(tvdbEps[tvdbSeason]).length
@@ -155,18 +177,37 @@ var matchEpisodeInfo = function(title, tvdbEps, xbmcEps) {
     } else {
       return true;
     }
-  }).sort(compareNum).forEach(function(tvdbSeason) {
-    var tvdbEpisodes = Object.keys(tvdbEps[tvdbSeason]).map(toInt);
-    tvdbEpisodes.filter(function(tvdbEpisode) {
-      return !xbmcEps[tvdbSeason].hasOwnProperty(tvdbEpisode);
-    }).sort(compareNum).forEach(function(tvdbEpisode) {
-      missingEpisodes.push({
-        season: tvdbSeason,
-        number: tvdbEpisode,
-        title: tvdbEps[tvdbSeason][tvdbEpisode].title
+  };
+};
+
+var buildEpisodeWalker = function(tvdbEps, kodiEps, missingEpisodes) {
+  return function(tvdbSeason) {
+    Object.keys(tvdbEps[tvdbSeason])
+      .map(toInt)
+      .filter(function(tvdbEpisode) {
+        return !kodiEps[tvdbSeason].hasOwnProperty(tvdbEpisode);
+      })
+      .sort(compareNum)
+      .forEach(function(tvdbEpisode) {
+        missingEpisodes.push({
+          season: tvdbSeason,
+          number: tvdbEpisode,
+          title: tvdbEps[tvdbSeason][tvdbEpisode].title
+        });
       });
-    });
-  });
+  };
+};
+
+var matchEpisodeInfo = function(title, tvdbEps, kodiEps) {
+  var missingSeasons = [], missingEpisodes = [];
+  if (config.options.excludeOlder) {
+    removeOldEpisodes(title, tvdbEps, kodiEps);
+  }
+  Object.keys(tvdbEps)
+    .map(toInt)
+    .filter(buildSeasonFilter(tvdbEps, kodiEps, missingSeasons))
+    .sort(compareNum)
+    .forEach(buildEpisodeWalker(tvdbEps, kodiEps, missingEpisodes));
   return {
     seasons: missingSeasons,
     episodes: missingEpisodes
@@ -179,13 +220,11 @@ var processShow = function(data) {
     title: title,
     id: id
   });
-  return Q.all([
-    title,
-    tvdbGetEpisodes(title),
-    xbmcGetEpisodes(title, id)
-  ]).spread(matchEpisodeInfo).then(function(missing) {
-    events.emit('processed_show', _.assign({title: title}, missing));
-  });
+  return Q.all([title, tvdbGetEpisodes(title), kodiGetEpisodes(title, id)])
+    .spread(matchEpisodeInfo)
+    .then(function(missing) {
+      events.emit('processed_show', _.assign({title: title}, missing));
+    });
 };
 
 var processShows = function(shows) {
@@ -198,40 +237,23 @@ var processShows = function(shows) {
     });
 };
 
+var handleError = function(err) {
+  console.error(err.stack || JSON.stringify(err));
+  process.exit(1);
+};
+
 var run = function() {
-  var dfd = Q.defer();
-  var tvdbLangs = config.tvdb.languages || ['en'];
-  delete config.tvdb.languages;
-  tvdbs = tvdbLangs.map(function(lang) {
-    return new TVDB(_.assign({}, config.tvdb, {language: lang}));
-  });
-  events.emit('kodi_connecting');
-  xbmc = new XBMC.XbmcApi({
-    connection: new XBMC.TCPConnection(config.xbmc),
-    silent: true
-  });
-  xbmc.on('connection:open', function() {
-    events.emit('kodi_connected');
-    events.emit('kodi_listing_shows');
-    Q(xbmc.media.tvshows())
-      .then(function(shows) {
-        events.emit('kodi_listed_shows', {shows: shows});
-        return processShows(shows);
-      })
-      .then(dfd.resolve, dfd.reject);
-  });
-  xbmc.on('connection:error', dfd.reject);
-  return dfd.promise;
+  return Q.all([tvdbInit(), kodiConnect()])
+    .then(function() {
+      events.emit('kodi_listing_shows');
+      return kodi.getShows();
+    })
+    .then(function(shows) {
+      events.emit('kodi_listed_shows', {shows: shows});
+      return processShows(shows);
+    });
 };
 
 run()
-  .fail(function(err) {
-    console.error(err.stack || JSON.stringify(err));
-  })
-  .fin(function() {
-    events.emit('kodi_disconnecting');
-    Q(xbmc.disconnect())
-      .then(function() {
-        events.emit('kodi_disconnected');
-      });
-  });
+  .fail(handleError)
+  .fin(kodiDisconnect);
